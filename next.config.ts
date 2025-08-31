7b76c5f88404441bf4ecdb7f459200cd6ac630f1
Northgate Institution <northgateinstitution25@gmail.com>
2025-08-31 13:23:40 +0530

    Updated project files

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   images: {
-    domains: ['https://oxhapmbatiykgcoydiij.supabase.co'],
+    domains: ['oxhapmbatiykgcoydiij.supabase.co'],
   },
 }
 
